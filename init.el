f33c8b9d65f504424af4c9878103765f05ebe5a8
XieGang <tianbymy@163.com>
2014-01-22 15:42:10 +0800

    emacs 24 color-theme配置

---
+++ init.el
@@ -0,0 +1,7 @@
+(setq load-path (cons "~/.emacs.d/lisp" load-path))
+
+;; 设置color-themes
+(add-to-list 'load-path "~/.emacs.d/themes")
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-matrix)
