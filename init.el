2c665a4a7a722ed21a22e6d5b3f8790e2fa886f3
XieGang <tianbymy@163.com>
2014-01-22 16:11:53 +0800

    php-html 模式

--- init.el
+++ init.el
@@ -6,3 +6,22 @@
 (color-theme-initialize)
 (color-theme-matrix)
 (color-theme-sitaramv-nt)
+
+;; 在mac下显示中文字体
+(set-fontset-font
+(frame-parameter nil 'font)
+'han
+(font-spec :family "Hiragino Sans GB" ))
+
+;; 设置php-html-mode
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                  (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
+;;加载php-mode
+(require 'php-mode)
+;;根据文件扩展名自动php-mode
+(add-to-list 'auto-mode-alist '("\\.php[34]?\\'\\|\\.phtml\\'" . php-mode))
